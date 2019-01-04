3c37e95b88c608f80f2e4071a4d9c389ce4ce64f
Josue <jgisber@gmail.com>
2019-01-04 13:29:55 -0700

    Added outlets and Methods

--- MemeApp/ViewController.swift
+++ MemeApp/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var topToolBar: UIToolbar!
+	@IBOutlet weak var bottomToolBar: UIToolbar!
+	@IBOutlet weak var imageView: UIImageView!
+	@IBOutlet weak var cameraButton: UIBarButtonItem!
+	@IBOutlet weak var topTextField: UITextField!
+	@IBOutlet weak var bottomTextField: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-
+	@IBAction func pickImageFromLibrary(_ sender: Any) {
+	}
+	
+	
+	@IBAction func pickImageFromCamera(_ sender: Any) {
+	}
+	
+	
+	@IBAction func shareImage(_ sender: Any) {
+	}
+	
+	
+	@IBAction func cancelTask(_ sender: Any) {
+	}
 }
 
