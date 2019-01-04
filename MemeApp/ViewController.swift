6027e5157de17f49a464cde603e34922efd09260
Josue <jgisber@gmail.com>
2019-01-04 12:57:56 -0700

    Initial Commit

---
+++ MemeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeApp
+//
+//  Created by Josue Gisber on 1/4/19.
+//  Copyright © 2019 Mpixel Design & Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
